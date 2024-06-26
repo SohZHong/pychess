b68714825647974771937e19b22be39663da0a74
Soh Zhe Hong <sohzhehong09@gmail.com>
2024-06-26 11:21:24 +0800

    Modified frontend port

--- pychess-admin/server.js
+++ pychess-admin/server.js
@@ -66,7 +66,7 @@ app.use('/api/system', sysRouter);
 // Game Routes
 app.use('/api', gameRouter);
 // Logout route
-app.post('/api/logout', logout)
+app.post('/api/logout', logout);
 
 // Debugging: Catch unhandled routes
 app.use((req, res, next) => {

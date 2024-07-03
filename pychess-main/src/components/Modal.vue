c0744e51c20c1cf511a22c26a062d1ce77d6944f
Soh Zhe Hong <sohzhehong09@gmail.com>
2024-07-03 20:14:58 +0800

    Added Modal

---
+++ pychess-main/src/components/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <dialog
+    ref="dialog"
+    @close="visible = false"
+  >
+    <div
+    v-if="visible"
+    :class="{[props.classes]: props.classes}"
+    >
+      <slot></slot>
+    </div>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const dialog = ref<HTMLDialogElement>();
+const props = defineProps({
+  classes: {
+    type: String,
+    default: "",
+  },
+});
+
+const visible = ref(false);
+
+const showModal = () => {
+  dialog.value?.showModal();
+  visible.value = true;
+};
+// Expose values for easier usage
+defineExpose({
+  show: showModal,
+  close: (returnVal?: string): void => dialog.value?.close(returnVal),
+  visible,
+});
+</script>
